4471ce92403e83c574cfe5b545073b7db2b9ef9e
phaksino <100353544+phaksino@users.noreply.github.com>
2024-02-19 11:12:25 +0200

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 import '../styles/Contact2.css'; // Ensure you create this CSS file for styling
 import Footer from './Footer';
 
